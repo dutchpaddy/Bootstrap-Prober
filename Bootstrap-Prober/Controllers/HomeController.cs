f9fbd6363e92e813181a9e9ba0311ecd6b59b601
Eric van Berkel <vanberkel.eric@gmail.com>
2023-01-26 11:18:39 +0100

    Add project files.

---
+++ Bootstrap-Prober/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Bootstrap_Prober.Models;
+
+namespace Bootstrap_Prober.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Unequalcols()
+        {
+            return View();
+
+        }
+
+        public IActionResult Bootstrap52List_groups()
+        {
+            return View();
+
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
